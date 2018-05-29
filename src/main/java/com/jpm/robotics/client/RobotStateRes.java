ba3acae2e23ba5e0c60d0df9fac904c1bd1e7d09
Mithilesh Bhende <mithileshbhende@gmail.com>
2018-05-29 06:56:52 -0400

    Unwanted code removed

--- src/main/java/com/jpm/robotics/client/RobotStateRes.java
+++ src/main/java/com/jpm/robotics/client/RobotStateRes.java
@@ -2,15 +2,6 @@ package com.jpm.robotics.client;
 
 public class RobotStateRes {
 
-//	stateMap.put("BaseFlag", "Y");
-//	stateMap.put("ProceedToLockerFlag", "N");
-//	stateMap.put("LockerOpenedFlag", "N");
-//	stateMap.put("ReachedLockerFlag", "N");
-//	stateMap.put("ProceedToDeskFlag", "Y");
-//	stateMap.put("ReachedDeskFlag", "Y");
-//	stateMap.put("DeliveredFlag", "Y");
-//
-//	
 	private String atBase;
 	private String proceedToLocker;
 	private String lockerOpened;
@@ -69,71 +60,5 @@ public class RobotStateRes {
 	public void setPackageDelivered(String packageDelivered) {
 		this.packageDelivered = packageDelivered;
 	}
-	@Override
-	public String toString() {
-		return "RobotStateRes [atBase=" + atBase + ", proceedToLocker=" + proceedToLocker + ", lockerOpened="
-				+ lockerOpened + ", reachedLocker=" + reachedLocker + ", proceedToDesk=" + proceedToDesk
-				+ ", reachedDesk=" + reachedDesk + ", packageDelivered=" + packageDelivered + "]";
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((atBase == null) ? 0 : atBase.hashCode());
-		result = prime * result + ((lockerOpened == null) ? 0 : lockerOpened.hashCode());
-		result = prime * result + ((packageDelivered == null) ? 0 : packageDelivered.hashCode());
-		result = prime * result + ((proceedToDesk == null) ? 0 : proceedToDesk.hashCode());
-		result = prime * result + ((proceedToLocker == null) ? 0 : proceedToLocker.hashCode());
-		result = prime * result + ((reachedDesk == null) ? 0 : reachedDesk.hashCode());
-		result = prime * result + ((reachedLocker == null) ? 0 : reachedLocker.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		RobotStateRes other = (RobotStateRes) obj;
-		if (atBase == null) {
-			if (other.atBase != null)
-				return false;
-		} else if (!atBase.equals(other.atBase))
-			return false;
-		if (lockerOpened == null) {
-			if (other.lockerOpened != null)
-				return false;
-		} else if (!lockerOpened.equals(other.lockerOpened))
-			return false;
-		if (packageDelivered == null) {
-			if (other.packageDelivered != null)
-				return false;
-		} else if (!packageDelivered.equals(other.packageDelivered))
-			return false;
-		if (proceedToDesk == null) {
-			if (other.proceedToDesk != null)
-				return false;
-		} else if (!proceedToDesk.equals(other.proceedToDesk))
-			return false;
-		if (proceedToLocker == null) {
-			if (other.proceedToLocker != null)
-				return false;
-		} else if (!proceedToLocker.equals(other.proceedToLocker))
-			return false;
-		if (reachedDesk == null) {
-			if (other.reachedDesk != null)
-				return false;
-		} else if (!reachedDesk.equals(other.reachedDesk))
-			return false;
-		if (reachedLocker == null) {
-			if (other.reachedLocker != null)
-				return false;
-		} else if (!reachedLocker.equals(other.reachedLocker))
-			return false;
-		return true;
-	}
-	
 	
 }
